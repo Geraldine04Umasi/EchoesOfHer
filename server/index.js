d66e11141a5f139d282279303e53c69da469bfbe
Geraldine Umasi <gumasi@unsa.edu.pe>
2026-03-08 06:28:20 -0500

    fix env for production

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const Story = require("./models/story");
 const OpenAI = require("openai").default;  
 
